ae43bf8accea73084f61c6e7ff2eedc11cbf63d4
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-06-25 15:03:11 +0900

    update project

---
+++ src/main/java/com/rest/API/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.rest.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
+
+// Resource
+// https://www.freecodecamp.org/news/how-to-build-a-rest-api-with-spring-boot-using-mysql-and-jpa-f931e348734b/
---
+++ src/main/java/com/rest/API/exception/BookNotFoundException.java
@@ -0,0 +1,8 @@
+package com.rest.API.exception;
+
+public class BookNotFoundException extends Exception{
+    public BookNotFoundException(long book_id) {
+        super(String.format("Book is not found with id : '%s'", book_id));
+
+    }
+}
